c8e8ffc40331a5f32b9b5ce1222515c38ac2f83f
Bryan Middleton <bryanmiddleton1986@gmail.com>
2023-10-24 18:47:14 +0100

    refactor with more independent attempt at code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Doctype javascript
 
+/* Attempt 1
 //module to create the gameboard
 const board = (function(){
 let gameBoard = new Array(9); //3x3 grid
@@ -14,16 +15,7 @@ for(let i=0;i<gameBoard.length;i++){
     //add eventlistener to allow square to be selected (still withing steup for loop)
     const squareID = document.getElementById(div.id);
     
-    //function startGame(){
-        const startGameBtn = document.getElementById("startGame");
-        startGameBtn.addEventListener("click", ()=>{
-            console.table(gameBoard);
-            gameBoard =[];
-            
-            console.table(gameBoard);
-        });
-    //}
-
+    
     squareID.addEventListener("click",() =>{
         console.log(game.activePlayer);
         //check if square has already been played
@@ -32,7 +24,7 @@ for(let i=0;i<gameBoard.length;i++){
         }
         else{
             gameBoard[i]=game.activePlayer.marker;
-            console.table(gameBoard);
+            //console.table(gameBoard);
             squareID.textContent=gameBoard[i]; //display value stored in array (x or o)
             game.remainingMoves-=1; //reduce number of remaining moves each time a turn is played
             
@@ -48,8 +40,15 @@ for(let i=0;i<gameBoard.length;i++){
             }
         }
     });
-    }
-  return{gameBoard}
+
+    const startGameBtn = document.getElementById("startGame");
+    startGameBtn.addEventListener("click", ()=>{
+       gameBoard[i]="";
+       squareID.textContent="";
+    });
+}
+
+return{gameBoard}
 })();
 
 
@@ -70,8 +69,8 @@ const game = (()=>{
 
     //declare the players
 
-    const player1=newPlayer(prompt("Enter Player 1 Name"),"X");
-    const player2=newPlayer(prompt("Enter Player 2 Name"), "O");
+    const player1=newPlayer(document.querySelector("playerOneName"),"X");
+    const player2=newPlayer(document.querySelector("playerTwoName"), "O");
 
     //initial game state
     let activePlayer = player1;
@@ -106,9 +105,89 @@ const game = (()=>{
     function drawn(){
         console.log("Game is a draw!");
     }
-
+    
    
     return{activePlayer,checkWinner,nextPlayer,winnerDeclared, remainingMoves, drawn}
 })();
-//placeholder - how to create a new player
+*/
+
+
+//Attempt 2
+//get page elements
+
+const startButton = document.getElementById("startGame");
+const playerOneInput = document.getElementById("playerOneName");
+const playerTwoInput = document.getElementById("playerTwoName");
+
+
+//factory function to create player
+
+function newPlayer(name, marker){
+    //hidden variable storing score
+    let score = 0;
+    //function to be called to add to score when game won
+    const increaseScore = () => score++;
+    //function to retrieve current score;
+    const getScore = () => score;
+
+    return {name, marker, score, getScore, increaseScore};
+}
+
+
+
+//array for board. 9 elements for 3x3 grid. 3 columns set in css file
+let gameBoard = new Array(9);
+
+//draw the board
+function drawBoard(){
+    for(let i=0;i<gameBoard.length;i++){
+        const div = document.createElement("div"); //create new div in each iteration of loop, 9 in total
+        div.classList.add("grid-item"); //add grid-item class for css styling
+        div.setAttribute("id","square"+ i); //give each square a unique ID
+        div.textContent = gameBoard[i]; //display the move stored in the box
+        document.getElementById("grid-container").appendChild(div); //add the new div to the page
+    }
+}
+
+//play the game
+
+const game = (()=>{
+    //initilise player variables
+
+    const playerOne = newPlayer(playerOneInput, "X");
+    const playerTwo = newPlayer(playerTwoInput, "O");
+
+    //empty the game board
+    gameBoard = [];
+    
+    //intial game state
+    let winnerDeclared = false;
+    let activePlayer = playerOne;
+    let remainingMoves = 9;
+
+    //winning combinations to be compared against
+    const winningCombo=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+    function checkWinner(){
+        //loop through the winningCombo array to check if there's a match for the current player
+        winningCombo.forEach((item)=>{
+            if(gameBoard[item[0]] === this.activePlayer.marker && board.gameBoard[item[1]] === this.activePlayer.marker && board.gameBoard[item[2]] === this.activePlayer.marker){
+                console.log(this.activePlayer.name + " wins!");
+                this.winnerDeclared=true;
+            }
+        });
+    }
+    //switch active player
+    function switchPlayer(){
+        if(thisactivePlayer==playerOne){
+            this.activePlayer=playerTwo;
+        }
+        else{
+            this.activePlayer=playerOne;
+        }
+    }
+    return{winnerDeclared,activePlayer,remainingMoves,checkWinner,switchPlayer}
+})();
+
+
 
